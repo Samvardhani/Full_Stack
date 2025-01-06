476b6ac65e9f040ded5c5ecb7fe9e8d01efd0300
Samvardhani <samvardhanid.22cse@kongu.edu>
2025-01-06 10:36:37 +0530

    frontend

---
+++ kecComputing/src/Components/Samplefunc/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>This is my Contact Page</h2>
+      <p>
+        <strong>Name:</strong> Samvardhani Dharmalingam
+      </p>
+      <p>
+        <strong>Email:</strong> samvardhanid.22cse@kongu.edu
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
